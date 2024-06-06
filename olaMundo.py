6e5ff1fd52d659eab198aec97e51d7fe43c4f4bb
Silas Bastianelli Pinto <89368427+silasbastianelli@users.noreply.github.com>
2024-06-06 14:14:21 -0300

    Add files via upload

---
+++ olaMundo.py
@@ -0,0 +1,7 @@
+nome= "Silas Bastianelli"
+idade = 33
+cidade = "Mogi das Cruzes -SP"
+formacao= "Estudando Python"
+
+print(f"Ola me chamo {nome} e tenho {idade} anos" )
+print(f"Moro em {cidade} e estou atualmente estudando  {formacao}." )
